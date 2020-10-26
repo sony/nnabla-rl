f2002582838b16638f38d2d1c1412a4b2f9cac3b
Yu Ishihara <ishihara-y@users.noreply.github.com>
2020-10-26 23:08:08 +0900

    Add deepcopy function to model to ease creating target model (#3)
    * Add deepcopy function to model to ease creating target model
    
    * Add model docstring
    
    * Add model copied parameter non equality test
    
    * Remove debug log prints
    
    * Fix doc typo

--- nnabla_rl/models/model.py
+++ nnabla_rl/models/model.py
@@ -1,4 +1,8 @@
+import copy
+
 import nnabla as nn
+
+from nnabla_rl.logger import logger
 
 import pathlib
 
@@ -9,20 +13,76 @@ class Model(object):
 
     @property
     def scope_name(self):
+        '''scope_name
+        Get scope name of this model.
+
+        Returns:
+            scope_name (str): scope name of the model
+        '''
         return self._scope_name
 
-    def get_parameters(self):
+    def get_parameters(self, grad_only=True):
+        '''get_parameters
+        Retrive parameters associated with this model
+
+        Args:
+            grad_only (bool): Retrive parameters only with need_grad = True. Defaults to True.
+
+        Returns:
+            parameters (OrderedDict): Parameter map.
+        '''
         with nn.parameter_scope(self.scope_name):
-            return nn.get_parameters()
+            return nn.get_parameters(grad_only=grad_only)
 
     def save_parameters(self, filepath):
+        '''save_parameters
+        Save model parameters to given filepath.
+
+        Args:
+            filepath (str or pathlib.Path): paramter file path
+        '''
         if isinstance(filepath, pathlib.Path):
             filepath = str(filepath)
         with nn.parameter_scope(self.scope_name):
             nn.save_parameters(path=filepath)
 
     def load_parameters(self, filepath):
+        '''load_parameters
+        Load model parameters from given filepath.
+
+        Args:
+            filepath (str or pathlib.Path): paramter file path
+        '''
         if isinstance(filepath, pathlib.Path):
             filepath = str(filepath)
         with nn.parameter_scope(self.scope_name):
             nn.load_parameters(path=filepath)
+
+    def deepcopy(self, new_scope_name):
+        '''deepcopy
+        Create a copy of the model. All the model parameter's (if exist) associated with will be copied.
+
+        Args:
+            new_scope_name (str): scope_name of parameters for newly created model
+
+        Returns:
+            Model: copied model
+
+        Raises:
+            ValueError: Given scope name is same as the model or already exists.
+        '''
+
+        if new_scope_name == self._scope_name:
+            raise ValueError('Can not use same scope_name!')
+        copied = copy.deepcopy(self)
+        copied._scope_name = new_scope_name
+        # copy current parameter if is already created
+        params = self.get_parameters(grad_only=False)
+        with nn.parameter_scope(new_scope_name):
+            for param_name, param in params.items():
+                if nn.parameter.get_parameter(param_name) is not None:
+                    raise RuntimeError(f'Model with scope_name: {new_scope_name} already exists!!')
+                logger.info(
+                    f'copying param with name: {self.scope_name}/{param_name} ---> {new_scope_name}/{param_name}')
+                nn.parameter.get_parameter_or_create(param_name, shape=param.shape, initializer=param.d)
+        return copied
--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,9 +1,35 @@
 import pytest
 
+import numpy as np
+
+import nnabla as nn
+import nnabla.functions as NF
+import nnabla.parametric_functions as NPF
+
 from nnabla_rl.models.model import Model
 
 
+class ModelMock(Model):
+    def __init__(self, scope_name, input_dim, output_dim):
+        super(ModelMock, self).__init__(scope_name)
+        self._input_dim = input_dim
+        self._output_dim = output_dim
+
+    def __call__(self, s):
+        assert s.shape[-1] == self._input_dim
+        with nn.parameter_scope(self.scope_name):
+            h = NPF.affine(s, n_outmaps=10, name="linear1")
+            h = NF.relu(x=h)
+            h = NPF.affine(h, n_outmaps=20, name="linear2")
+            h = NF.relu(x=h)
+            h = NPF.affine(h, n_outmaps=self._output_dim, name="linear3")
+        return NF.tanh(h)
+
+
 class TestModel(object):
+    def setup_method(self, method):
+        nn.clear_parameters()
+
     def test_scope_name(self):
         scope_name = "test"
         model = Model(scope_name=scope_name)
@@ -15,6 +41,98 @@ class TestModel(object):
         model = Model(scope_name=scope_name)
 
         assert len(model.get_parameters()) == 0
+
+    def test_deepcopy_without_model_initialization(self):
+        scope_name = 'src'
+        input_dim = 5
+        x = nn.Variable.from_numpy_array(np.empty(shape=(1, input_dim)))
+        model = self._create_model_from_input(scope_name=scope_name, x=x)
+
+        new_scope_name = 'copied'
+        copied = model.deepcopy(new_scope_name)
+        assert type(copied) is type(model)
+        assert len(model.get_parameters()) == 0
+        assert len(copied.get_parameters()) == 0
+
+    def test_deepcopy_model_is_same(self):
+        scope_name = 'src'
+        input_dim = 5
+        x = nn.Variable.from_numpy_array(np.ones(shape=(1, input_dim)))
+        model = self._create_model_from_input(scope_name=scope_name, x=x)
+
+        # Call once to create params
+        model(x)
+
+        new_scope_name = 'copied'
+        copied = model.deepcopy(new_scope_name)
+        assert type(copied) is type(model)
+
+    def test_deepcopy_model_parameters_are_not_shared(self):
+        scope_name = 'src'
+        input_dim = 5
+        x = nn.Variable.from_numpy_array(np.ones(shape=(1, input_dim)))
+        model = self._create_model_from_input(scope_name=scope_name, x=x)
+
+        # Call once to create params
+        model(x)
+
+        new_scope_name = 'copied'
+        copied = model.deepcopy(new_scope_name)
+
+        for src_value in model.get_parameters().values():
+            for dst_value in copied.get_parameters().values():
+                assert src_value is not dst_value
+
+    def test_deepcopy_model_has_same_param_num(self):
+        scope_name = 'src'
+        input_dim = 5
+        x = nn.Variable.from_numpy_array(np.ones(shape=(1, input_dim)))
+        model = self._create_model_from_input(scope_name=scope_name, x=x)
+
+        # Call once to create params
+        expected = model(x)
+
+        new_scope_name = 'copied'
+        copied = model.deepcopy(new_scope_name)
+        assert len(copied.get_parameters()) == len(model.get_parameters())
+
+        actual = copied(x)
+
+        nn.forward_all([expected, actual])
+        # Should output same value
+        assert np.allclose(expected.d, actual.d)
+
+    def test_deepcopy_same_scope_name_not_allowed(self):
+        scope_name = 'src'
+        input_dim = 5
+        x = nn.Variable.from_numpy_array(np.empty(shape=(1, input_dim)))
+        model = self._create_model_from_input(scope_name=scope_name, x=x)
+
+        # Call once to create params
+        model(x)
+
+        with pytest.raises(ValueError):
+            model.deepcopy(scope_name)
+
+    def test_deepcopy_cannot_create_with_existing_scope_name(self):
+        scope_name = 'src'
+        input_dim = 5
+        x = nn.Variable.from_numpy_array(np.empty(shape=(1, input_dim)))
+        model = self._create_model_from_input(scope_name=scope_name, x=x)
+
+        # Call once to create params
+        model(x)
+
+        new_scope_name = 'new'
+        model.deepcopy(new_scope_name)
+
+        # Can not create with same scope twice
+        with pytest.raises(RuntimeError):
+            model.deepcopy(new_scope_name)
+
+    def _create_model_from_input(self, scope_name, x, output_dim=5):
+        input_dim = x.shape[-1]
+        return ModelMock(scope_name=scope_name, input_dim=input_dim, output_dim=output_dim)
 
 
 if __name__ == "__main__":
