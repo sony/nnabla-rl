1fe10bb297c28f9dcc8624dbcfd5c8e9e5e88c2f
Shunichi Sekiguchi <72590119+sbsekiguchi@users.noreply.github.com>
2021-04-13 16:29:46 +0900

    update docs of files (#91)

--- nnabla_rl/utils/files.py
+++ nnabla_rl/utils/files.py
@@ -18,14 +18,11 @@ import os
 def file_exists(path):
     '''
     Check file existence on given path
-    Parameters
-    -------
-    path : string
-        Path of the file to check existence
-    Returns
-    -------
-    file_existence : bool
-        True if file exists otherwise False
+
+    Args:
+        path (str or pathlib.Path): Path of the file to check existence
+    Returns:
+        file_existence (bool): True if file exists otherwise False
     '''
     return os.path.exists(path)
 
@@ -33,14 +30,11 @@ def file_exists(path):
 def create_dir_if_not_exist(outdir):
     '''
     Check directory existence and creates new directory if not exist
-    Parameters
-    -------
-    outdir : string
-        Path of the file to create directory
-    Raises
-    ------
-    RuntimeError
-        File exists in outdir but it is not a directory
+
+    Args:
+        outdir (str or pathlib.Path): Path of the file to create directory
+    Raises:
+        RuntimeError: File exists in outdir but it is not a directory
     '''
     if file_exists(outdir):
         if not os.path.isdir(outdir):
@@ -53,14 +47,11 @@ def create_dir_if_not_exist(outdir):
 def read_text_from_file(file_path):
     '''
     Read given file as text
-    Parameters
-    -------
-    file_path : string
-        Path of the file to read data
-    Returns
-    -------
-    data: string
-        Text read from the file
+
+    Args:
+        file_path (str or pathlib.Path): Path of the file to read data
+    Returns:
+        data (str): Text read from the file
     '''
     with open(file_path, 'r') as f:
         return f.read()
@@ -69,12 +60,10 @@ def read_text_from_file(file_path):
 def write_text_to_file(file_path, data):
     '''
     Write given text data to file
-    Parameters
-    -------
-    file_path : string
-        Path of the file to write data
-    data: string
-        Text to write to the file
+
+    Args:
+        file_path (str or pathlib.Path): Path of the file to write data
+        data (str): Text to write to the file
     '''
     with open(file_path, 'w') as f:
         f.write(data)
